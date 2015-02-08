7b3815a7097411fe4a6e17382efd322af54c0695
FireF0re <fischy525@gmail.com>
2015-02-08 18:34:46 +0100

    Befehl /ton hinzugefügt

--- src/me/firef0re/Test.java
+++ src/me/firef0re/Test.java
@@ -1,8 +1,31 @@
 package me.firef0re;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Test extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		System.out.println("[Test] Plugin aktiviert!");
+	}
+	
+	public void onDisable() {
+		System.out.println("[Test] Plugin deaktiviert!");
+	}
 
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args) {
+		Player p = (Player) Bukkit.getOnlinePlayers();
+		
+		if (cmd.getName().equalsIgnoreCase("ton")) {
+			p.playSound(p.getLocation(), Sound.ANVIL_USE, 30, 30);
+			
+			return false;
+		}
+		return false;
+	}
 }
