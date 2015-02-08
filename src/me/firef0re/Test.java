236adc1e789b4aafab3098f7d5ab1aa8ab51ea08
FireF0re <fischy525@gmail.com>
2015-02-08 22:12:29 +0100

    Commit

--- src/me/firef0re/Test.java
+++ src/me/firef0re/Test.java
@@ -1,10 +1,5 @@
 package me.firef0re;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,16 +11,5 @@ public class Test extends JavaPlugin implements Listener {
 	
 	public void onDisable() {
 		System.out.println("[Test] Plugin deaktiviert!");
-	}
-
-	public boolean onCommand(CommandSender sender, Command cmd,String cmdlabel, String[] args) {
-		Player p = (Player) Bukkit.getOnlinePlayers();
-		
-		if (cmd.getName().equalsIgnoreCase("ton")) {
-			p.playSound(p.getLocation(), Sound.ANVIL_USE, 30, 30);
-			
-			return true;
-		}
-		return false;
 	}
 }
